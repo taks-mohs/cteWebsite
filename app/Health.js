ea152e68653c22d2cf8c4da9f18e266f6b6f04aa
BannaDanna <JSTing08@gmail.com>
2025-02-19 12:01:06 -1000

    Health Template

--- app/Health.js
+++ app/Health.js
@@ -1,25 +1,51 @@
-import { ImageBackground, Text, View, styles, ScrollView} from "react-native-web"
-import topBar from "../components/topBar"
+import { ImageBackground, Text, View, StyleSheet, ScrollView, Pressable} from "react-native-web"
+import TopBar from "../components/topBar"
 import { Link } from "expo-router"
+import Colors from "../components/colors"
 
 
 export default function Health() {
     return (
         <View style={styles.background}>
-        //     <topB />
-        //     <ImageBackground style={styles.bgImg} source={require('../assets/engineer.png')}>
-        //         <ScrollView>
-
-        //             <View style={styles.chunk1}>
-        //                 <Text style={styles.c1Title}>Find Your Pathway</Text>
-        //                 <Link href={'/searchingtool'}>
-        //                     <Pressable>
-        //                         <Text style={styles.c1Button}>Searching Tool</Text>
-        //                     </Pressable>
-        //                 </Link>
-        //             </View>
-        //         </ScrollView>
-        //     </ImageBackground>
+             <TopBar />
+             <ImageBackground style={styles.bgImg} source={require('../assets/engineer.png')}>
+                 <ScrollView>
+                     <View style={styles.chunk1}>
+                         <Text style={styles.c1Title}>Health Services</Text>
+                     </View>
+                 </ScrollView>
+             </ImageBackground>
         </View>
     )
 }
+
+export const styles = StyleSheet.create({
+    background: {
+      flex: 1,
+      backgroundColor: Colors.primary,
+    },
+    bgImg: {
+      height: '100%',
+      width: '100%',
+      resizeMode: 'cover'
+    },
+    chunk1: {
+      marginTop: 500,
+      marginHorizontal: 150,
+      padding: 75,
+      backgroundColor: Colors.primary,
+      alignItems: 'center'
+    },
+    c1Title: {
+      fontSize: 75,
+      color: Colors.secondary,
+      fontFamily: 'oswaldmedium',
+      marginBottom: 25
+    },
+    c1Button: {
+      fontSize: 25,
+      padding: 10,
+      color: Colors.primary,
+      backgroundColor: Colors.secondary,
+      fontFamily: 'oswaldsemibold'
+    }})

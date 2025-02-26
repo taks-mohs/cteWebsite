22b54922c6ee48eaed42b502c898b61ea52c15bc
ORANGEDUCKEN <aidenmak07@gmail.com>
2025-02-26 09:12:44 -1000

    update aboutpqage

--- app/about.js
+++ app/about.js
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image, ScrollView } from 'react-native'
 import TopBar from '../components/topBar'
 import Colors from '../components/colors'
 import { useFonts, Oswald_300Light, Oswald_600SemiBold, Oswald_500Medium } from '@expo-google-fonts/oswald'
@@ -12,13 +12,39 @@ export default function deptpage() {
   return (
     <View>
       <TopBar />
-      <View style={styles.container}>
-        <Text style={styles.font}>
-          Career and Technological Education (CTE) is all about empowering students with the knowledge and skills they need to thrive in the modern workforce. It bridges the gap between traditional academic education and the practical, hands-on experience required in various industries.
-          CTE programs are designed to prepare students for a wide range of career paths, from healthcare and information technology to engineering, culinary arts, and beyond. By offering a blend of classroom instruction and real-world experience, CTE helps students develop critical thinking, problem-solving, and technical skills that are directly applicable to their chosen fields.
-          These programs often include opportunities for internships, apprenticeships, and industry certifications, giving students a competitive edge in the job market. By participating in CTE, students gain a deeper understanding of their career interests, build a professional network, and acquire the skills needed to succeed in today's dynamic and evolving job landscape. Overall, Career and Technological Education provides a valuable foundation for students to achieve their career goals, contribute to the economy, and lead fulfilling, productive lives.
-        </Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <View style={styles.container}>
+          <Text style={styles.font}>
+            For Moanalua High School, Career and Technological Education (CTE) is all about empowering students with the knowledge and skills they need to thrive in the modern workforce. It bridges the gap between traditional academic education and the practical, hands-on experience required in various industries.
+            CTE programs are designed to prepare students for a wide range of career paths, from healthcare and information technology to engineering, culinary arts, and beyond. By offering a blend of classroom instruction and real-world experience, CTE helps students develop critical thinking, problem-solving, and technical skills that are directly applicable to their chosen fields.
+            These programs often include opportunities for internships, apprenticeships, and industry certifications, giving students a competitive edge in the job market. By participating in CTE, students gain a deeper understanding of their career interests, build a professional network, and acquire the skills needed to succeed in today's dynamic and evolving job landscape. Overall, Career and Technological Education provides a valuable foundation for students to achieve their career goals, contribute to the economy, and lead fulfilling, productive lives.
+            <br />
+            <br />
+            Credits: Kelly, Gabe, Gabby, Christian, Jeremiah, Josh, Aiden
+            <br />
+            <View style={styles.collage}>
+              <Image
+                source={require('../assets/placeholder.jpg')}
+                style={styles.img}
+              />
+              <Image
+                source={require('../assets/placeholder.jpg')}
+                style={styles.img}
+              />
+              <Image
+                source={require('../assets/placeholder.jpg')}
+                style={styles.img}
+              />
+              <Image
+                source={require('../assets/placeholder.jpg')}
+                style={styles.img}
+              />
+            </View>
+
+          </Text>
+
+        </View>
+      </ScrollView>
     </View>
   )
 }
@@ -30,10 +56,25 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: Colors.primary,
-    
+
+  },
+  scrollContent: {
+    flexGrow: 1,
+    padding: 20,
+    height: 460,
   },
   font: {
     fontFamily: 'oswaldlight',
     fontSize: 20,
+  },
+  img: {
+    width: 250,
+    height: 250,
+    
+  },
+  collage: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
---
+++ app/results.js
@@ -0,0 +1,27 @@
+import { Text, View, StyleSheet, ImageBackground } from 'react-native'
+import TopBar from '../components/topBar'
+import Colors from '../components/colors'
+
+export default function deptpage() {
+  return (
+    <View>
+      <TopBar />
+      <View style={styles.container}>
+        <Text>
+            hi
+        </Text>
+
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 250,
+    marginHorizontal: 100,
+    flex: 1,
+    padding: 20,
+    backgroundColor: Colors.primary,
+  }
+})

7da3ed37400c9a00376e2b13c6bcd94cb2e7a6d2
Adob0 <Gabeyere3@gmail.com>
2025-03-11 11:52:44 -1000

    Working on Computer Science Branch = Teacher portraits and working on the description of each classes, etc.

--- app/CS.js
+++ app/CS.js
@@ -16,36 +16,46 @@ export default function CS() {
                         <ScrollView style={{ height: 500 }} showsVerticalScrollIndicator={false}>
                             <View style={styles.break}></View>
                             <View style={styles.c2chunk}>
-                                <Text style={styles.c2cHeader}>Class 1</Text>
+                                <Text style={styles.c2cHeader}>Found. Comp. Sys & Tech</Text>
                                 <Image
                                     source={require('../assets/placeholder.jpg')}
                                     style={styles.studentImg}
                                 />
                                 <Text style={styles.c2cBody}>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at vulputate enim, in dictum neque.
-                                    Aliquam iaculis pharetra maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                                    Computer science concepts covered will include an exploration
+                                    of computer programming in different applications such as web development, game development and robotics. This course is a recommended pre-cursor to the AP Computer Science A course.
+                                    Recommended for students who are considering a major in computer science or who will want to be informed citizens in today’s technological
+                                    society.
                                 </Text>
                             </View>
                             <View style={styles.c2chunk}>
-                                <Text style={styles.c2cHeader}>Class 2</Text>
+                                <Text style={styles.c2cHeader}>AP Computer Science</Text>
                                 <Image
                                     source={require('../assets/placeholder.jpg')}
                                     style={styles.studentImg}
                                 />
                                 <Text style={styles.c2cBody}>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at vulputate enim, in dictum neque.
-                                    Aliquam iaculis pharetra maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                                    Students are introduced to computer science with
+                                    fundamental topics that include problem solving, design strategies and methodologies, organization of data (data structures), approaches to
+                                    processing data (algorithms), analysis of potential solutions, and the ethical and social implications of computing. The course emphasizes
+                                    both object-oriented and imperative problem solving and design using Java language. These techniques represent proven approaches for
+                                    developing solutions that can scale up from small, simple problems to large, complex problems.
+                                    In May, students will be required to take the AP Computer Science A examination given by the College Board. On the basis of this
+                                    examination, the student’s college will determine how much advanced placement and/or credit the student will receive. There is a required
+                                    fee for the AP exam
                                 </Text>
                             </View>
                             <View style={styles.c2chunk}>
-                                <Text style={styles.c2cHeader}>Class 3</Text>
+                                <Text style={styles.c2cHeader}>Programming 2: Mobile App Development</Text>
                                 <Image
                                     source={require('../assets/placeholder.jpg')}
                                     style={styles.studentImg}
                                 />
                                 <Text style={styles.c2cBody}>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at vulputate enim, in dictum neque.
-                                    Aliquam iaculis pharetra maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                                    In May, students will be required to take the AP Computer Science A examination given by the College Board. On the basis of this
+                                    examination, the student’s college will determine how much advanced placement and/or credit the student will receive. There is a required
+                                    fee for the AP exam.
+                                    Required: Successful completion of AP Computer Science A
                                 </Text>
                             </View>
                         </ScrollView>
@@ -54,14 +64,15 @@ export default function CS() {
                         <ScrollView style={{ height: 500 }} showsVerticalScrollIndicator={false}>
                             <View style={styles.break}></View>
                             <View style={styles.c2chunk}>
-                                <Text style={styles.c2cHeader}>Club Activity 1</Text>
+                                <Text style={styles.c2cHeader}>Menedevs</Text>
                                 <Image
                                     source={require('../assets/placeholder.jpg')}
                                     style={styles.studentImg}
                                 />
                                 <Text style={styles.c2cBody}>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at vulputate enim, in dictum neque.
-                                    Aliquam iaculis pharetra maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                                    Menedevs juggles between Website Development, Game Development, and Machine Learning. This year saw the club focus on Game Development, working within Game Engine, Unity.
+                                    However, next year, Menedevs might switch over to Website Development.
+                                    Website Development next year.
                                 </Text>
                             </View>
                             <View style={styles.c2chunk}>
@@ -94,25 +105,34 @@ export default function CS() {
                         <ScrollView style={{ height: 500 }} showsVerticalScrollIndicator={false}>
                             <View style={styles.break}></View>
                             <View style={styles.c2chunk}>
-                                <Text style={styles.c2cHeader}>Takahashi</Text>
+                                <Text style={styles.c2cHeader}>S. Takahashi</Text>
                                 <Image
-                                    source={require('../assets/placeholder.jpg')}
+                                    source={require('../assets/CS/GOAT.jpg')}
                                     style={styles.studentImg}
                                 />
                                 <Text style={styles.c2cBody}>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at vulputate enim, in dictum neque.
-                                    Aliquam iaculis pharetra maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                                    Both a math teacher and computer science teacher. Mr.Takashi teaches Introduction to Computer Science, AP Computer Science A, and Programming 2: Mobile Apps Development
+                                    He might also teach other computer science classes.
                                 </Text>
                             </View>
                             <View style={styles.c2chunk}>
-                                <Text style={styles.c2cHeader}>Teacher 2</Text>
+                                <Text style={styles.c2cHeader}>B. Park</Text>
                                 <Image
-                                    source={require('../assets/placeholder.jpg')}
+                                    source={require('../assets/CS/Park.jpg')}
                                     style={styles.studentImg}
                                 />
                                 <Text style={styles.c2cBody}>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at vulputate enim, in dictum neque.
-                                    Aliquam iaculis pharetra maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                                    Both a math teacher and computer science teacher. One of the other Introduction to Computer Science teachers
+                                </Text>
+                            </View>
+                            <View style={styles.c2chunk}>
+                                <Text style={styles.c2cHeader}>H. Ishikawa</Text>
+                                <Image
+                                    source={require('../assets/CS/Ishikawa.jpg')}
+                                    style={styles.studentImg}
+                                />
+                                <Text style={styles.c2cBody}>
+                                    Both a math teacher and computer science teacher. One of the other Introduction to Computer Science teachers.
                                 </Text>
                             </View>
                         </ScrollView>
@@ -125,69 +145,69 @@ export default function CS() {
 
 export const styles = StyleSheet.create({
     background: {
-      flex: 1,
-      backgroundColor: Colors.primary,
+        flex: 1,
+        backgroundColor: Colors.primary,
     },
     bgImg: {
-      height: '100%',
-      width: '100%',
-      resizeMode: 'cover'
+        height: '100%',
+        width: '100%',
+        resizeMode: 'cover'
     },
     chunk1: {
-      marginTop: 200,
-      marginHorizontal: 150,
-      padding: 75,
-      backgroundColor: Colors.primary,
-      alignItems: 'center'
+        marginTop: 200,
+        marginHorizontal: 150,
+        padding: 75,
+        backgroundColor: Colors.primary,
+        alignItems: 'center'
     },
     c1Title: {
-      fontSize: 75,
-      color: Colors.secondary,
-      fontFamily: 'oswaldmedium',
-      marginBottom: 25
+        fontSize: 75,
+        color: Colors.secondary,
+        fontFamily: 'oswaldmedium',
+        marginBottom: 25
     },
     chunk2: {
-      marginTop: 100,
-      padding: 100,
-      marginBottom: 150,
-      height: 500,
-      backgroundColor: Colors.primary,
-      alignItems: 'center',
-      flexDirection: 'row'
+        marginTop: 100,
+        padding: 100,
+        marginBottom: 150,
+        height: 500,
+        backgroundColor: Colors.primary,
+        alignItems: 'center',
+        flexDirection: 'row'
     },
     c2title: {
-      fontSize: 75,
-      marginRight: 250,
-      color: Colors.secondary,
-      fontFamily: 'oswaldsemibold'
+        fontSize: 75,
+        marginRight: 250,
+        color: Colors.secondary,
+        fontFamily: 'oswaldsemibold'
     },
     c3title: {
-      fontSize: 75,
-      marginLeft: 250,
-      color: Colors.secondary,
-      fontFamily: 'oswaldsemibold'
+        fontSize: 75,
+        marginLeft: 250,
+        color: Colors.secondary,
+        fontFamily: 'oswaldsemibold'
     },
     break: {
-      margin: 40
+        margin: 40
     },
     c2chunk: {
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      marginBottom: 75
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginBottom: 75
     },
     c2cBody: {
-      fontSize: 18,
-      fontFamily: 'oswaldlight',
-      textAlign: 'center'
+        fontSize: 18,
+        fontFamily: 'oswaldlight',
+        textAlign: 'center'
     },
     studentImg: {
-      height: 250,
-      width: 250
+        height: 250,
+        width: 250
     },
     c2cHeader: {
-      fontSize: 40,
-      color: Colors.secondary,
-      fontFamily: 'oswaldmedium'
+        fontSize: 40,
+        color: Colors.secondary,
+        fontFamily: 'oswaldmedium'
     }
-  })
+})

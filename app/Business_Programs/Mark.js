e392661df43cd6b8b58a0ef3b8913ed2d762c8a0
BannaDanna <JSTing08@gmail.com>
2025-03-25 09:31:21 -1000

    Added Business Program Pages

--- app/Business_Programs/Mark.js
+++ app/Business_Programs/Mark.js
@@ -1,10 +1,55 @@
-import { View } from "react-native-web"
-import TopBar from "../../components/topBar"
+import { ImageBackground, Text, View, StyleSheet, ScrollView } from "react-native-web";
+import TopBar from "../../components/topBar";
+import Colors from "../../components/colors";
 
-export default function Mark() {
+export default function Entre() {
     return (
-        <View>
+        <View style={styles.background}>
             <TopBar />
+            <ImageBackground style={styles.bgImg} source={require('../../assets/placeholder.jpg')}>
+                <ScrollView>
+                    <View style={styles.chunk1}>
+                        <Text style={styles.c1Title}>Marketing</Text>
+                    </View>
+                    <View style={styles.chunk2}>
+
+                    </View>
+                </ScrollView>
+            </ImageBackground>
         </View>
     )
 }
+
+export const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: Colors.primary,
+    },
+    bgImg: {
+        height: '100%',
+        width: '100%',
+        resizeMode: 'cover',
+    },
+    chunk1: {
+        marginTop: 200,
+        marginHorizontal: 150,
+        padding: 75,
+        backgroundColor: Colors.primary,
+        alignItems: 'center'
+    },
+    c1Title: {
+        fontSize: 75,
+        color: Colors.secondary,
+        fontFamily: 'oswaldmedium',
+        marginBottom: 25
+    },
+    chunk2: {
+        marginTop: 100,
+        padding: 100,
+        marginBottom: 150,
+        height: 500,
+        backgroundColor: Colors.primary,
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+})

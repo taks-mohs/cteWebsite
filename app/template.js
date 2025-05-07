0e8164e46ef0e49724d113cc821b33b0f1e56b09
bingle <kelly1281100989@gmail.com>
2025-05-07 09:46:00 -1000

    template descriptions

--- app/template.js
+++ app/template.js
@@ -21,6 +21,11 @@ export default function template() {
       resizeMode: 'cover'
     }
   })
+
+  /* 
+  This is an EXAMPLE page.
+  Components are meant to be used freely and this page is not intended to be fully copy & pasted.
+  */
   
   return (
     <View style={styles.background}>
@@ -28,23 +33,24 @@ export default function template() {
       <ImageBackground style={styles.bgImg} source={require('../assets/placeholder.jpg')}>
         <ScrollView>
           <View style={{marginTop: width * 0.05}}></View>
-          <HeaderChunk
-          header="HeaderChunk"
+          <HeaderChunk //chunk with background padding and header
+          header="HeaderChunk" //the text displayed on the header
           />
           <View style ={{marginTop: width * 0.05}}></View>
-          <Chunk1
-            header="Chunk 1"
-            buttonlabel="Button"
-            link="/"
+          <Chunk1 //chunk with background padding and header with a button underneath
+            header="Chunk 1" //the text displayed on the header
+            buttonlabel="Button" //the text displayed on the button
+            link="/" //the link to the page the button leads to
           />
           <View style={{marginTop: width* 0.05}}></View>
-          <Chunk2L 
-            header="Chunk 2L"
+          <Chunk2L //chunk with header and scrollable chunk2info component(s)
+            header="Chunk 2L" //the text displayed on the hedaer
             info={[
-                <Chunk2Info
-                  infoHeader="Info 1"
-                  image={require('../assets/placeholder.jpg')}
+                <Chunk2Info //a chunk within the chunk2 component (each block requires one chunk2info component)
+                  infoHeader="Info 1" //the text displayed on the header
+                  image={require('../assets/placeholder.jpg')} //the image displayed under the header
                   infoBody="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                  //the text displayed under the image
                 />,
                 <Chunk2Info
                   infoHeader="Info 2"
@@ -59,7 +65,7 @@ export default function template() {
               ]}
           />
           <View style={{marginTop: width* 0.05}}></View>
-          <Chunk2R
+          <Chunk2R //works the same as chunk2L, but the hedaer is on the right
             header="Chunk 2R"
             info={[
                 <Chunk2Info
